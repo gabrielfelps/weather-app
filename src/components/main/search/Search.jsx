304e7d308ea0f46fb6fc9b3c8a71cf2258ce8760
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-03-28 01:10:11 -0300

    feat(search): make search bar a controlled component and improve accessibility

--- src/components/main/search/Search.jsx
+++ src/components/main/search/Search.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import SearchBar from './SearchBar';
 import SearchButton from './SearchButton';
 
 function Search() {
+  const [searchQuery, setsearchQuery] = useState('');
+
+  const onSearchChange = (e) => {
+    setsearchQuery(e.target.value);
+  };
+
   return (
     <div className="flex flex-col gap-4 w-full mx-auto md:flex-row md:justify-between lg:max-w-158">
-      <SearchBar />
+      <SearchBar searchQuery={searchQuery} onSearchChange={onSearchChange} />
       <SearchButton />
     </div>
   );
--- src/components/main/search/SearchBar.jsx
+++ src/components/main/search/SearchBar.jsx
@@ -1,15 +1,21 @@
 import icon_search from '../../../assets/images/icon-search.svg';
 
-function SearchBar() {
+function SearchBar({ searchQuery, onSearchChange }) {
   return (
-    <div className="flex flex-1 items-center text-xl self-stretch gap-4 px-6 py-4 rounded-xl bg-neutral-800 focus-within:shadow-[0_0_0_3px_#02012C,0_0_0_5px_#fff] transform-gpu">
+    <label
+      htmlFor="countryInput"
+      className="flex flex-1 items-center text-xl self-stretch gap-4 px-6 py-4 rounded-xl bg-neutral-800 focus-within:shadow-[0_0_0_3px_#02012C,0_0_0_5px_#fff] cursor-text transform-gpu"
+    >
       <img src={icon_search} alt="Search icon" />
       <input
-        className="placeholder-neutral-200 placeholder:text-xl outline-hidden"
+        className="flex-1 placeholder-neutral-200 placeholder:text-xl outline-hidden"
         type="text"
+        id="countryInput"
+        value={searchQuery}
+        onChange={onSearchChange}
         placeholder="Search for a place..."
       />
-    </div>
+    </label>
   );
 }
 
