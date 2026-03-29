a0784210a11b3f9ba422b265ba8b187d6f78120a
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-03-29 14:28:33 -0300

    feat(search): connect search button to weather data fetching

--- src/components/main/search/Search.jsx
+++ src/components/main/search/Search.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import SearchBar from './SearchBar';
 import SearchButton from './SearchButton';
+import { WeatherDataContext } from '../../../context/WeatherDataContext';
 
 function Search() {
   const [searchQuery, setsearchQuery] = useState('');
+  const { fetchWeatherData } = useContext(WeatherDataContext);
 
   const onSearchChange = (e) => {
     setsearchQuery(e.target.value);
   };
 
+  const handleClick = () => {
+    if (!searchQuery) return;
+
+    fetchWeatherData(searchQuery);
+  };
+
   return (
     <div className="flex flex-col gap-4 w-full mx-auto md:flex-row md:justify-between lg:max-w-158">
       <SearchBar searchQuery={searchQuery} onSearchChange={onSearchChange} />
-      <SearchButton />
+      <SearchButton onClick={handleClick} />
     </div>
   );
 }
