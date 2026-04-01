9409a774d440ffc993f6d1dc8923db1523438083
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-04-01 12:42:02 -0300

    refactor(components): extract DetailCard from WeatherDetails to improve modularity

---
+++ src/components/main/weather/DetailCard.jsx
@@ -0,0 +1,12 @@
+function DetailCard({ title, metrics }) {
+  return (
+    <div className="flex flex-col p-5 items-start gap-6 bg-neutral-800 border border-neutral-600 rounded-xl">
+      <p className="text-neutral-200 text-center text-lg font-medium leading-[120%]">
+        {title}
+      </p>
+      <p className="text-[2rem] font-light leading-[120%]">{metrics}</p>
+    </div>
+  );
+}
+
+export default DetailCard;
--- src/components/main/weather/WeatherDetails.jsx
+++ src/components/main/weather/WeatherDetails.jsx
@@ -1,3 +1,5 @@
+import DetailCard from './DetailCard';
+
 // Temporary code to test the component layout.
 const details = [
   { id: 1, title: 'Feels Like', metrics: '18°' },
@@ -10,15 +12,7 @@ export default function WeatherDetails() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-5">
       {details.map(({ id, title, metrics }) => (
-        <div
-          className="flex flex-col p-5 items-start gap-6 bg-neutral-800 border border-neutral-600 rounded-xl"
-          key={id}
-        >
-          <p className="text-neutral-200 text-center text-lg font-medium leading-[120%]">
-            {title}
-          </p>
-          <p className="text-[2rem] font-light leading-[120%]">{metrics}</p>
-        </div>
+        <DetailCard key={id} title={title} metrics={metrics} />
       ))}
     </div>
   );
