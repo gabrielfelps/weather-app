e86b543b2eba52fcb55fb67d9fcd6c7311fb04cf
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-04-17 10:56:51 -0300

    feat(utils): add dynamic weekday formatting to formatDate utility

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,8 +1,8 @@
-export default function formatDate(dateString) {
+export default function formatDate(dateString, weekDayValue = 'short') {
   const date = dateString + 'T12:00:00';
   const dateObj = new Date(date);
 
-  const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'short' });
+  const formatter = new Intl.DateTimeFormat('en-US', { weekday: weekDayValue });
 
   return formatter.format(dateObj);
 }
