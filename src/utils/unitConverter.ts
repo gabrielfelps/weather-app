47315e5ec0a8e6ca8b563b2d9da538e8de4ebb40
gabrielfelps <gabrielfelps.dev@gmail.com>
2026-05-18 22:24:55 -0300

    refactor(utils): migrate unit conversion functions to typescript

---
+++ src/utils/unitConverter.ts
@@ -0,0 +1,11 @@
+export const celsiusToFahrenheit = (celsius: number) => {
+  return (celsius * 9) / 5 + 32;
+};
+
+export const kmhToMph = (kmh: number) => {
+  return kmh * 0.621371;
+};
+
+export const mmToInch = (mm: number) => {
+  return mm / 25.4;
+};
